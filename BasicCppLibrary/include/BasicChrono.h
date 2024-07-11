a2fb97b8797afdadf1a0a2eccdfb3ce7c4304354
Giulio M <1172898+giuliom@users.noreply.github.com>
2024-07-11 22:22:27 +0100

    Fix template parameter error

--- BasicCppLibrary/include/BasicChrono.h
+++ BasicCppLibrary/include/BasicChrono.h
@@ -9,7 +9,7 @@ namespace bsc
 		template<typename OutputType, typename DurationMeasure, typename DurationType>
 		OutputType convert_chrono_duration(const DurationType& duration)
 		{
-			return std::chrono::duration_cast<std::chrono::duration<OutputType, DurationMeasure::period>>(duration).count();
+			return std::chrono::duration_cast<std::chrono::duration<OutputType, typename DurationMeasure::period>>(duration).count();
 		}
 	}
 }
